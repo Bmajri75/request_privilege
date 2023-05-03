b46f83346aaf2bdfa466010ee4605f89da5c2972
messerschmitt755 <ryan.legasal@gmail.com>
2023-05-03 16:17:35 +0200

    starting contact page

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,12 +1,34 @@
 import style from './Contact.module.scss'
 import Header from '../../Header/Header'
 
-
 function Contact() {
   return (
     <div>
       <Header/>
-      <h1>CONTACT</h1>
+      <div className={`${style.container}`} >
+        <div className={`${style.titleContainer}`} >
+          <h2 className={`${style.title}`} >Formulaire de contact</h2>
+        </div>
+        <h4 className={`${style.subtitle}`}>Contactez Request & Privilege via ce formulaire ou à l'adresse: contact@requestandprivilegeconciergerie.com</h4>
+        <form>
+          <div>
+            <h4>Votre prénom</h4>
+            <input/>
+          </div>
+          <div>
+            <h4>Votre nom</h4>
+            <input />
+          </div>
+          <div>
+            <h4>Objet de votre demande</h4>
+            <input/>
+          </div>
+          <div className={`${style.yourMessage}`} >
+            <h4>Votre message</h4>
+            <input />
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
